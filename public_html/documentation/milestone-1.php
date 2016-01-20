6bede92717413c168296d32cb8c4579cd546dea6
Victoria Hooker <vhooker@cnm.edu>
2016-01-20 08:02:08 -0700

    CSS Not working Got some content done.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -16,7 +16,7 @@
 						Name: Victoria Senolee Hooker<br>
 						Born: Route 66, Zuzax Exit. It's now a Park and Ride. Ionic, isn't it?<br>
 						Career: Creator<br>
-						Education: Self-taught BA in Life, going for my Masters. If that doesn't kill me, I'll start thinking about my PHD.<br>
+						Education: Self-taught BS in Life, going for my Masters. If that doesn't kill me, I'll start thinking about my PHD.<br>
 						Ethnicity: I'm so mixed, I call myself American.<br>
 						Life's Lesson: If you are depressed, you are living in the past. If you are anxious, you are living in the future. Neither is in your control. Live in the moment and do the very best you can. In it's own way, it does soften the past and makes for a stronger tomorrow. <br>
 
