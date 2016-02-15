4f77fe46e2b00a9e9f90edc6b0bdc1520297617c
Victoria Hooker <vhooker@cnm.edu>
2016-02-14 19:08:08 -0700

    Began the resume

---
+++ public_html/documentation/Resume.php
@@ -0,0 +1,8 @@
+Core
+Competencies		●	Creative marketing visionary; skilled in graphic and web design, databases, social media and building strong business alliances in the community
+●	Skilled administrator; experienced in corporate restructuring and nonprofit expansion, policy and procedure development
+●	15 years of management experience; including hiring, training, supervision and performance reviews of staff and volunteers
+●	Excellent communication and phone skills; specializing in quality customer care. Ability to defuse conflicts and complaints while providing service excellence
+●	Lifelong scholar; passion for educating self and others to achieve goals. Adapt to new concepts quickly and efficiently
+●	Reliable and responsible self-starter; effective leader/collaborator in team-based projects. High level of personal integrity and dedication for completion of self-directed projects
+
